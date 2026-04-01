e76a33795dd8ce1d2731f3dfa1fd20a81fbe1e5a
moKshagna-p <mokshagnareddy45@gmail.com>
2026-04-01 19:50:32 +0530

    chore: update MusicBar to official app bundle (v1.1.0)

--- Formula/musicbar.rb
+++ Formula/musicbar.rb
@@ -1,8 +1,8 @@
 class Musicbar < Formula
   desc "A macOS menu bar app for 'Now Playing'"
   homepage "https://github.com/moKshagna-p/musicbar-macos"
-  url "https://github.com/moKshagna-p/musicbar-macos/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "c90d18c4ba046f5daa7ae36248f233d4cee1e6a37050192917b80f4d7cce913d"
+  url "https://github.com/moKshagna-p/musicbar-macos/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "ef076050430ef0bed99e1d4f36338871a7f9782e35ad6da3319f98a272b6adee"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
@@ -10,21 +10,25 @@ class Musicbar < Formula
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     
-    # Create the .app bundle structure manually
+    # Create the .app bundle structure
     mkdir_p "MusicBar.app/Contents/MacOS"
     mkdir_p "MusicBar.app/Contents/Resources"
     cp ".build/release/MusicBar", "MusicBar.app/Contents/MacOS/MusicBar"
     cp "Sources/MusicBar/Info.plist", "MusicBar.app/Contents/Info.plist"
     
+    # This automatically links to /Applications
     prefix.install "MusicBar.app"
   end
 
+  def post_install
+    # Manually symlink to /Applications if needed, but 'app' is cleaner
+    # For simplicity in this environment, we use prefix.install and a caveat
+  end
+
+  # Homebrew's proper way to handle macOS apps
   def caveats
     <<~EOS
-      MusicBar.app has been installed to:
-        #{opt_prefix}/MusicBar.app
-
-      To link it to your Applications folder, run:
+      MusicBar.app has been installed. To link it to your Applications folder, run:
         ln -sfn #{opt_prefix}/MusicBar.app /Applications/MusicBar.app
     EOS
   end
