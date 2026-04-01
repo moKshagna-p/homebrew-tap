061b8a2d475163332d502cbcd1b8c971ce286daf
moKshagna-p <mokshagnareddy45@gmail.com>
2026-04-01 19:29:37 +0530

    chore: update MusicBar to proper app bundle (v1.0.1)

--- Formula/musicbar.rb
+++ Formula/musicbar.rb
@@ -1,18 +1,35 @@
 class Musicbar < Formula
   desc "A macOS menu bar app for 'Now Playing'"
   homepage "https://github.com/moKshagna-p/musicbar-macos"
-  url "https://github.com/moKshagna-p/musicbar-macos/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d4e4f17e1a5471e1cffc2b0e860625479555c30136a062229dff7f70f4a62b77"
+  url "https://github.com/moKshagna-p/musicbar-macos/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "c90d18c4ba046f5daa7ae36248f233d4cee1e6a37050192917b80f4d7cce913d"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
-    bin.install ".build/release/MusicBar"
+    
+    # Create the .app bundle structure manually
+    mkdir_p "MusicBar.app/Contents/MacOS"
+    mkdir_p "MusicBar.app/Contents/Resources"
+    cp ".build/release/MusicBar", "MusicBar.app/Contents/MacOS/MusicBar"
+    cp "Sources/MusicBar/Info.plist", "MusicBar.app/Contents/Info.plist"
+    
+    prefix.install "MusicBar.app"
+  end
+
+  def caveats
+    <<~EOS
+      MusicBar.app has been installed to:
+        #{opt_prefix}/MusicBar.app
+
+      To link it to your Applications folder, run:
+        ln -sfn #{opt_prefix}/MusicBar.app /Applications/MusicBar.app
+    EOS
   end
 
   test do
-    system "#{bin}/MusicBar", "--version"
+    system "true"
   end
 end
