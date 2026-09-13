28fee7cff3fb208dc3391e11cd27adef4ad3d8f1
moKshagna-p <mokshagnareddy45@gmail.com>
2026-09-13 12:20:37 +0530

    lrc 0.1.1

--- Formula/lrc.rb
+++ Formula/lrc.rb
@@ -1,12 +1,12 @@
 class Lrc < Formula
   desc "Full-screen terminal UI for synced Apple Music and Spotify lyrics"
   homepage "https://github.com/moKshagna-p/lrc"
-  url "https://github.com/moKshagna-p/lrc/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3f956d2ad3246a792d5dc8cd0ad0cae89c5e498934de7cb9451e52ad0008a54d"
+  url "https://github.com/moKshagna-p/lrc/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "381658458d06a4492ba77c6b21f0162b852fed267e90b57968ac98cdf80b39fe"
   license :cannot_represent
 
-  depends_on :macos
   depends_on "go" => :build
+  depends_on :macos
 
   def install
     ldflags = "-s -w -X main.version=#{version}"
